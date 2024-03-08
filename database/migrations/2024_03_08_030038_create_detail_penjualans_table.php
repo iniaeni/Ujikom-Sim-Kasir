61049f9c3613b40c22328cb6e77e02ad9ec01032
@siti.nuraeni <sitinuraen20@gmail.com>
2024-03-08 10:11:58 +0700

    upload db

---
+++ database/migrations/2024_03_08_030038_create_detail_penjualans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->biginteger('penjualan_id');
+            $table->biginteger('produk_id');
+            $table->integer('jumlah_produk');
+            $table->float('sub_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_penjualans');
+    }
+};
